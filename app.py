8b35b87c429a67367ed9d05538b5315dae2560c5
yashwanthreddys007 <yashwanthreddys557@gmail.com>
2026-03-15 17:12:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ form_filter = st.sidebar.multiselect(
     default=["elite", "good"]
 )
 value_only = st.sidebar.checkbox("Value picks only", value=False)
-top_n = st.sidebar.slider("Show top N players", 10, 600, 15)
+top_n = st.sidebar.slider("Show top N players", 10, 822, 100)
 
 # --- FILTER ---
 filtered = df.copy()
